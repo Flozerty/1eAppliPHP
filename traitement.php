03f3b02aa65625e8001a93a28776220e88c52fef
Flozerty <floris.louerat@gmail.com>
2024-03-18 10:57:06 +0100

    htmlspecialchars fix

--- traitement.php
+++ traitement.php
@@ -5,7 +5,7 @@ session_start();
 if(isset($_POST['submit'])){
 
   // $name = filter_input(INPUT_POST,'name', FILTER_SANITIZE_STRING); (deprecated)
-  $name = htmlspecialchars($_POST['name'], ENT_QUOTES);
+  $name = htmlspecialchars($_POST['name']);
   $price = filter_input(INPUT_POST,'price', FILTER_VALIDATE_FLOAT, FILTER_FLAG_ALLOW_FRACTION);
   $qtt = filter_input(INPUT_POST,'qtt', FILTER_VALIDATE_INT);
 
@@ -14,9 +14,10 @@ if(isset($_POST['submit'])){
       'name'=> $name,
       'price'=> $price,
       'qtt'=> $qtt,
-      'total' => $qtt*$price
+      'total' => $qtt * $price
     ];
 
+    // initialisation  de la clé "products" si elle est inexistante dans le tableau de $_SESSION, puis, ajoute le "product" dans le tableau.
     $_SESSION['products'][] = $product;
   }
 }
